846bcb41cf24a1acd5b1f2433d2216829abb2c4e
utkarshh0110 <utkarshh0110@gmail.com>
2021-04-28 13:51:52 +0530

    encrypt and decrypt

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Axios from "axios";
 
 const App = () => {
   const [password, setPassword] = useState("");
   const [title, setTitle] = useState("");
+  const [passwordList, setPasswordList] = useState([]);
 
-  const addPassword = () => {
-    Axios.post("http://localhost:5000/addpassword", { password, title });
+  useEffect(() => {
+    Axios.get("http://localhost:5000/showpasswords").then((response) => {
+      setPasswordList(response.data);
+    });
+  }, []);
+
+  const addPassword = async () => {
+    await Axios.post("http://localhost:5000/addpassword", { password, title });
+    setPassword("");
+    setTitle("");
+    await Axios.get("http://localhost:5000/showpasswords").then((response) => {
+      setPasswordList(response.data);
+    });
+  };
+
+  const decryptPassword = (encryption) => {
+    Axios.post("http://localhost:5000/decryptPassword", {
+      password: encryption.password,
+      iv: encryption.iv,
+    }).then((response) => {
+      setPasswordList(passwordList.map((val) => {
+        return val.id === encryption.id ? {id: val.id, password: val.password, title: `Password :- ${response.data}`, iv:val.iv} : val;
+      }))
+    });
+
+
   };
 
   return (
@@ -17,13 +42,31 @@ const App = () => {
           onChange={(e) => setPassword(e.target.value)}
           type="text"
           placeholder="Ex. password123"
+          value={password}
         />
         <input
           onChange={(e) => setTitle(e.target.value)}
           type="text"
           placeholder="Ex. Facebook"
+          value={title}
         />
         <button onClick={addPassword}>Add Password</button>
+      </div>
+
+      <div className="Passwords">
+        {passwordList.map((val) => {
+          return (
+            <div
+              key={val.id}
+              className="password"
+              onClick={() =>
+                decryptPassword({ password: val.password, iv: val.iv, id:val.id })
+              }
+            >
+              <h3>{val.title}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
+const { decrypt, encrypt } = require("./EncryptionHandler");
 
 app.use(cors());
 app.use(express.json());
@@ -11,23 +12,25 @@ const PORT = 5000;
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "admin",
-  database: "passtest",
+  password: "",
+  database: "",
 });
 
-db.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.message);
-      return;
-    }})
+db.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.message);
+    return;
+  }
+});
 
 app.post("/addpassword", (req, res) => {
   const { password, title } = req.body;
-  console.log(password, title)
+  const hashedPassword = encrypt(password);
+
 
   db.query(
-    "INSERT INTO manager (password, title) VALUES (?,?)",
-    [password, title],
+    "INSERT INTO manager (password, title, iv) VALUES (?,?,?)",
+    [hashedPassword.password, title, hashedPassword.iv],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -37,6 +40,21 @@ app.post("/addpassword", (req, res) => {
     }
   );
 });
+
+app.get("/showpasswords", (req, res) => {
+  db.query("SELECT * FROM manager;", (err, result) => {
+    if(err){
+      console.log(err)
+      return res.send("NO PASSWORD FOUND....")
+    }
+    res.send(result)
+  })
+})
+
+
+app.post("/decryptPassword", (req,res) => {
+  res.send(decrypt(req.body));
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
